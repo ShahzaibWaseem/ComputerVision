136a34abcd62b0b39eee25a69b41b4efdbacf4ed
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2022-01-21 23:02:22 -0800

    inner product backwards implemented

--- Assignment 1/matlab/inner_product_backward.m
+++ Assignment 1/matlab/inner_product_backward.m
@@ -2,4 +2,9 @@ function [param_grad, input_od] = inner_product_backward(output, input, layer, p
 	% Replace the following lines with your implementation.
 	param_grad.b = zeros(size(param.b));
 	param_grad.w = zeros(size(param.w));
+
+	input_od = param.w * output.diff;
+	
+	param_grad.w = input.data * output.diff';
+	param_grad.b = param_grad.b + output.diff';
 end
