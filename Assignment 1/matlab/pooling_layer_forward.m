88353dcda24a33a1feb50ba57b58a6fbffa2e1d8
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2022-01-21 21:14:01 -0800

    comments

--- Assignment 1/matlab/pooling_layer_forward.m
+++ Assignment 1/matlab/pooling_layer_forward.m
@@ -17,12 +17,14 @@ function [output] = pooling_layer_forward(input, layer)
 
 	% Replace the following line with your implementation.
 	output.data = zeros([h_out, w_out, channel, batch_size]);
+
 	for batch = 1:batch_size
 		image = reshape(input.data(:, batch), h_in, w_in, channel);
 		image = padarray(image, [pad pad]); % Image Processing Toolbox required for this
 		for c = 1:channel
 			for w = 1:w_out
 				for h = 1:h_out
+					% pooling matrix is extracted from the input data
 					pool_window = image((h-1)*stride+1 : (h-1)*stride+k, (w-1)*stride+1 : (w-1)*stride+k, c);
 					output.data(h, w, c, batch) = max(pool_window, [], "all");
 				end % width
