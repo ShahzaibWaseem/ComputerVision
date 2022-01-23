d103ca85980fe6cfa0dab8675678d0e286d770c0
Shahzaib Waseem <msw10@sfu.ca>
2022-01-22 23:39:18 -0800

    confusion matrix implemented

--- Assignment 1/matlab/test_network.m
+++ Assignment 1/matlab/test_network.m
@@ -10,6 +10,17 @@ load lenet.mat
 
 %% Testing the network
 % Modify the code to get the confusion matrix
+prediction = [];
+actual = [];
 for i=1:100:size(xtest, 2)
 	[output, P] = convnet_forward(params, layers, xtest(:, i:i+99));
+	[~, pred] = max(P);		% max value, argmax
+	actual = [actual, ytest(:, i:i+99)];
+	prediction = [prediction, pred];
 end
+
+confusion = confusionmat(actual, prediction);
+disp(confusion);
+
+confusionchart(confusion);
+saveas(gcf, "../results/confusion.png");
