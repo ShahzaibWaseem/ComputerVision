dc6d5176340f82cceec670e27a087fa6df8bfbfd
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2022-01-30 13:40:16 -0800

    visualize ReLU and Convolution layer feature maps

--- Assignment 1/matlab/vis_data.m
+++ Assignment 1/matlab/vis_data.m
@@ -12,9 +12,26 @@ batch_size = 64;
 layers{1}.batch_size = 1;
 img = xtest(:, 1);
 img = reshape(img, 28, 28);
-imshow(img')
 
-%[cp, ~, output] = conv_net_output(params, layers, xtest(:, 1), ytest(:, 1));
 output = convnet_forward(params, layers, xtest(:, 1));
-output_1 = reshape(output{1}.data, 28, 28);
+
 % Fill in your code here to plot the features.
+output_1 = reshape(output{1}.data, 28, 28);
+conv = reshape(output{2}.data', 24, 24, []);
+relu = reshape(output{3}.data', 24, 24, []);
+
+f1 = figure();
+sgtitle("Convolution");
+for plot = 1:4*5
+	subplot(4, 5, plot);
+	imshow(conv(:, :, plot)');
+end
+saveas(f1, "../results/convolution.png");
+
+f2 = figure();
+sgtitle("ReLU");
+for plot = 1:4*5
+	subplot(4, 5, plot);
+	imshow(relu(:, :, plot)');
+end
+saveas(f2, "../results/relu.png");
