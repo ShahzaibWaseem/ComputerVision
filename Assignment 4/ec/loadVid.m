a47e467e83a6c9337a90e5bfc40f786141260f95
Shahzaib Waseem <msw10@sfu.ca>
2022-03-21 18:15:41 -0700

    corrected comments and standardizing

--- Assignment 4/ec/loadVid.m
+++ Assignment 4/ec/loadVid.m
@@ -8,8 +8,8 @@ function [mov] = loadVid(path)
 	mov(1:nFrames) = struct("cdata", zeros(vidHeight, vidWidth, 3, "uint8"), "colormap", []);
 
 	% Read one frame at a time.
-	for k = 1 : nFrames
-		fprintf("%d/%d\n",k,nFrames);
+	for k = 1:nFrames
+		fprintf("%d/%d\n", k, nFrames);
 		mov(k).cdata = read(data, k);
 	end
 end
