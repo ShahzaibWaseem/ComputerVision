f15fbe906079b48ae611e8a2b7f0d649c75bfb55
Shahzaib Waseem <msw10@sfu.ca>
2022-03-21 18:13:16 -0700

    (correction) replaced x, y with x_p, y_p

--- Assignment 4/matlab/computeH.m
+++ Assignment 4/matlab/computeH.m
@@ -8,13 +8,13 @@ function [H2to1] = computeH(x, x_prime)
 	A = zeros(2 * size(x, 1), 9);
 
 	for i = 1:size(x)
-		A(2*i-1:2*i, :) = [[-x(i), -y(i), -1, 0, 0, 0, x(i)*x_p(i), y(i)*x_p(i), x_p(i)]; ...
-						   [0, 0, 0, -x(i), -y(i), -1, x(i)*y_p(i), y(i)*y_p(i), y_p(i)]];
+		A(2*i-1:2*i, :) = [[-x_p(i), -y_p(i), -1, 0, 0, 0, x_p(i) * x(i), x(i) * y_p(i), x(i)]; ...
+						   [0, 0, 0, -x_p(i), -y_p(i), -1, y(i) * x_p(i), y(i) * y_p(i), y(i)]];
 	end
 	% Singular Vector (or eigenvector) with lowest corresponding Singular Value (or eigenvalue) is the solution
 	[~, ~, V] = svd(A);
 
 	% MATLAB has the lowest singular vector at the end
 	H2to1 = V(:, end);
-	H2to1 = reshape(H2to1, [3, 3]);
+	H2to1 = reshape(H2to1, [3, 3])';
 end
