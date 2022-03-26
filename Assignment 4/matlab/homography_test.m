5b5658aa368aa6435e7e4e599f484f0de4b4389a
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2022-03-25 19:12:13 -0700

    correcting the homography testing bug

--- Assignment 4/matlab/homography_test.m
+++ Assignment 4/matlab/homography_test.m
@@ -7,33 +7,32 @@ pano_right = imread("../data/pano_right.jpg");
 
 [locs1, locs2] = matchPics(pano_left, pano_right);
 
-idy = randi([1, size(pano_left, 1)], 1, 10);		% 10 random points
-idx = randi([1, size(pano_left, 2)], 1, 10);
+% using first 10 of these random points for homography testing
+idx = randperm(size(locs1, 1));
 
-rand_locs1 = [idx; idy]';
-locs1_appended = horzcat(rand_locs1, ones(size(rand_locs1, 1), 1))';
+[H2to1] = computeH(locs1(1:10, :), locs2(1:10, :));
 
-[H2to1] = computeH(locs1, locs2);
+locs1_appended = horzcat(locs1(11:20, :), ones(size(locs1(11:20, :), 1), 1))';
 
 locs2_gen = (H2to1 * locs1_appended)';
-locs2_gen = locs2_gen(:, 1:2) ./ locs2_gen(:, 3)
+locs2_gen = locs2_gen(:, 1:2) ./ locs2_gen(:, 3);
 
 %% Visualize the point-pairs.
 fig = figure; ax = axes;
-showMatchedFeatures(pano_left, pano_right, rand_locs1, locs2_gen, "montage", "Parent", ax);
+showMatchedFeatures(pano_left, pano_right, locs1(11:20, :), locs2_gen, "montage", "Parent", ax);
 legend(ax, "Matched points 1", "Matched points 2");
 title(ax, "Planar Homography");
 frame = getframe(fig);
 imwrite(frame2im(frame), "../results/homography.png");
 
-[H2to1] = computeH_norm(locs1, locs2);
+[H2to1] = computeH_norm(locs1(1:10, :), locs2(1:10, :));
 
 locs2_gen = (H2to1 * locs1_appended)';
-locs2_gen = locs2_gen(:, 1:2) ./ locs2_gen(:, 3)
+locs2_gen = locs2_gen(:, 1:2) ./ locs2_gen(:, 3);
 
 %% Visualize the point-pairs.
 fig = figure; ax = axes;
-showMatchedFeatures(pano_left, pano_right, rand_locs1, locs2_gen, "montage", "Parent", ax);
+showMatchedFeatures(pano_left, pano_right, locs1(11:20, :), locs2_gen, "montage", "Parent", ax);
 legend(ax, "Matched points 1", "Matched points 2");
 title(ax, "Normalized Planar Homography");
 frame = getframe(fig);
