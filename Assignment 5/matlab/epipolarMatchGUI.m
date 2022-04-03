486d83e9c0f1ed705b23aff17640135babfc5bd4
Shahzaib Waseem <msw10@sfu.ca>
2022-04-02 18:13:20 -0700

    standardizing yet again

--- Assignment 5/matlab/epipolarMatchGUI.m
+++ Assignment 5/matlab/epipolarMatchGUI.m
@@ -6,11 +6,11 @@ function [coordsIM1, coordsIM2] = epipolarMatchGUI(I1, I2, F)
 	figure(gcf), clf
 	h_axes1 = subplot(121);
 	imshow(I1, []); hold on
-	xlabel({"Select a point in this image", "(Right-click when finished)"})
+	xlabel({"Select a point in this image", "(Right-click when finished)"});
 
 	subplot 122
 	imshow(I2, []);
-	xlabel({"Verify that the corresponding point", "is on the epipolar line in this image"})
+	xlabel({"Verify that the corresponding point", "is on the epipolar line in this image"});
 
 	while 1
 		subplot 121
@@ -23,11 +23,11 @@ function [coordsIM1, coordsIM2] = epipolarMatchGUI(I1, I2, F)
 
 		if(gca ~= h_axes1)
 			subplot 121
-			title("Please click only in this image")
+			title("Please click only in this image");
 			continue;
 		else
 			subplot 121
-			title("")
+			title("");
 		end
 
 		xc = x;
@@ -39,7 +39,7 @@ function [coordsIM1, coordsIM2] = epipolarMatchGUI(I1, I2, F)
 
 		l = F * v';
 
-		s = sqrt(l(1) ^ 2+l(2) ^ 2);
+		s = sqrt(l(1) ^ 2 + l(2) ^ 2);
 
 		if s == 0
 			error("Zero line vector in displayEpipolar");
@@ -50,13 +50,13 @@ function [coordsIM1, coordsIM2] = epipolarMatchGUI(I1, I2, F)
 		if l(1) ~= 0
 			ye = sy;
 			ys = 1;
-			xe = -(l(2) * ye + l(3)) / l(1);
-			xs = -(l(2) * ys + l(3)) / l(1);
+			xe = - (l(2) * ye + l(3)) / l(1);
+			xs = - (l(2) * ys + l(3)) / l(1);
 		else
 			xe = sx;
 			xs = 1;
-			ye = -(l(1) * xe + l(3)) / l(2);
-			ys = -(l(1) * xs + l(3)) / l(2);
+			ye = - (l(1) * xe + l(3)) / l(2);
+			ys = - (l(1) * xs + l(3)) / l(2);
 		end
 
 		plot(x, y, "*", "MarkerSize", 10, "LineWidth", 2);
