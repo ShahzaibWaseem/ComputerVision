aa57b47f9c9057dc88481ce5a293d433970635e7
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2022-04-03 18:17:00 -0700

    implementing and testing essential matrix function

--- Assignment 5/matlab/essentialMatrix.m
+++ Assignment 5/matlab/essentialMatrix.m
@@ -4,7 +4,7 @@ function E = essentialMatrix(F, K1, K2)
 	%		F:	Fundamental Matrix
 	%		K1:	Camera Matrix 1
 	%		K2:	Camera Matrix 2
-	%
 	%	Returns:
 	%		E:	Essential Matrix
+	E = K2' * F * K1;
 end
--- Assignment 5/matlab/runner.m
+++ Assignment 5/matlab/runner.m
@@ -5,6 +5,7 @@ image1 = imread("../data/im1.png");
 image2 = imread("../data/im2.png");
 
 corresp = load("../data/someCorresp.mat");
+intrinsic = load("../data/intrinsics.mat");
 
 % Testing eightpoint algorithm
 F = eightpoint(corresp.pts1, corresp.pts2, corresp.M);
@@ -12,4 +13,7 @@ F = eightpoint(corresp.pts1, corresp.pts2, corresp.M);
 
 % Testing epipolarCorrespondence task
 pts2 = epipolarCorrespondence(image1, image2, F, corresp.pts1);
-[coords_im1, coords_im2] = epipolarMatchGUI(image1, image2, F);
+% [coords_im1, coords_im2] = epipolarMatchGUI(image1, image2, F);
+
+% Testing the essential matrix calculation
+E = essentialMatrix(F, intrinsic.K1, intrinsic.K2);
