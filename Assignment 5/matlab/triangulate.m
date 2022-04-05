afc1a1a90ab257dfa49db3524c86019892c50267
Shahzaib Waseem <msw10@sfu.ca>
2022-04-04 17:28:15 -0700

    triangulate function written and tested

--- Assignment 5/matlab/triangulate.m
+++ Assignment 5/matlab/triangulate.m
@@ -7,4 +7,16 @@ function pts3d = triangulate(P1, pts1, P2, pts2)
 	%		pts2:	coordinates of points with shape N x 2 on image 2
 	%	Returns:
 	%		Pts3d:	coordinates of 3D points with shape N x 3
+
+	pts3d = zeros(size(pts1, 1), 3);
+
+	for i=1:size(pts1, 1)
+		A = [pts1(i, 2) * P1(3, :) - P1(2, :);
+			 P1(1, :) - pts1(i, 1) * P1(3, :);
+			 pts2(i, 2) * P2(3, :) - P2(2, :);
+			 P2(1, :) - pts2(i, 1) * P2(3, :)];
+		[~, ~, V] = svd(A);
+		V = V(:, end)';
+		pts3d(i, :) = V(1:3) / V(4);
+	end
 end
