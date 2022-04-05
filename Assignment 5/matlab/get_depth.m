b5d68eb6724e3ca321eec2f39f474334cd9a6bac
Shahzaib Waseem <msw10@sfu.ca>
2022-04-05 16:04:25 -0700

    depth code according to the handout

--- Assignment 5/matlab/get_depth.m
+++ Assignment 5/matlab/get_depth.m
@@ -1,3 +1,14 @@
 function depthM = get_depth(dispM, K1, K2, R1, R2, t1, t2)
 	% GET_DEPTH creates a depth map from a disparity map (DISPM).
+	% t1n = -R1n * c1;
+	% t2n = -R2n * c2;
+
+	c1 = - inv(R1) * t1;
+	c2 = - inv(R2) * t2;
+	b = norm(c1 - c2);
+
+	f = K1(1, 1);
+
+	depthM = b * f ./ dispM;
+	depthM(isnan(depthM)) = 0;
 end
