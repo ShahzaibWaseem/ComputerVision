71df4e1e89c31b64f931e36916058faa207c669d
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2022-04-06 03:24:47 -0700

    fixing the order of cross multiplication

--- Assignment 5/matlab/rectify_pair.m
+++ Assignment 5/matlab/rectify_pair.m
@@ -10,14 +10,14 @@ function [M1, M2, K1n, K2n, R1n, R2n, t1n, t2n] = rectify_pair(K1, K2, R1, R2, t
 	% 2. compute the rotation matrix
 	r1 = (c1 - c2) / norm(c1 - c2);
 	r2 = cross(R1(3, :), r1)';
-	r3 = cross(r1, r2);
+	r3 = cross(r2, r1);
 
-	R1n = [r1 r2 r3];
+	R1n = [r1 r2 r3]';
 
 	r2 = cross(R2(3, :), r1)';
-	r3 = cross(r1, r2);
+	r3 = cross(r2, r1);
 
-	R2n = [r1 r2 r3];
+	R2n = [r1 r2 r3]';
 
 	% 3. compute the new intrinsic parameters
 	K1n = K1;
